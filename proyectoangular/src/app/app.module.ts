501a54e134790a759cd811544b718bf5d28863e3
JuanMelinir <juan.melinir@est.fi.uncoma.edu.ar>
2022-05-07 12:01:48 -0300

    empezando con los estilos de los componentes

--- proyectoangular/src/app/app.module.ts
+++ proyectoangular/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContentComponent,
-    SidemenuComponent
+    SidemenuComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
